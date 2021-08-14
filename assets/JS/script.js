f4a16f00574efd627bc4189339a4ae30eec3dd7b
kalyani joshi <kalyani121@gmail.com>
2021-08-14 09:43:23 -0400

    Added functionalities:1. Retrieve data from Local storage and add to saved events for the current day.2.  Disable Save options for past time. 3. Updated Stylesheet for highlighting current Date and Time.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,41 +1,73 @@
 var schedule = [];
 //Scheduled time starts from 9 AM to 5 PM
-const start_time = ["9", "10", "11", "12", "13", "14", "15", "16"];
+const start_time = [
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+];
 var today = null;
-var scheduleLocalStorage = null;
 
+//initiate function to get current date
 function init() {
   //Moment JS to get Today's date
   today = moment();
   $("#currentDay").text("Today is " + today.format("MMM Do, YYYY"));
+  $("#currentTime").text("Current time is " + today.format("hh:mm:ss A"));
+  console.log("Today is " + today.format("MMM Do, YYYY"));
 
-  scheduleLocalStorage = localStorage.getItem("dailySchedule");
   scheduleHighlight();
 }
 
+//Highlight schedule for past, present and current time
 function scheduleHighlight() {
+  let scheduleLocalStorage = JSON.parse(localStorage.getItem("dailySchedule"));
   start_time.forEach((t) => {
-    let txtarea = $(`#eventRow${t}`);
-    let currentHour = parseInt(today.format("HH"), 10) + 1;
+    let row = $(`#eventRow${t}`);
+    let currentHour = parseInt(today.format("HH:MM:ss A"), 10);
     if (currentHour === parseInt(t, 10)) {
-        txtarea.addClass("present");
+      row.addClass("present");
     } else if (currentHour < parseInt(t, 10)) {
-        txtarea.addClass("future");
+      row.addClass("future");
     } else {
-        txtarea.addClass("past");
+      row.addClass("past");
+      let btnEl = $(`#btn${t}`);
+      btnEl.attr("disabled", true);
     }
   });
+  
+  //Load schedule for from Local Storage
+  if (scheduleLocalStorage !== null || scheduleLocalStorage.length) {
+    let daily = scheduleLocalStorage.find(
+      (s) => s.date === today.format("MM/DD/yyyy")
+    );
+    if (daily) {
+      daily.schedule.forEach((s) => {
+        let event = $(`#event${s.time}`);
+        event.text(s.event);
+      });
+    }
+  }
 }
 
+//Save event details to local storage
 function saveEvent(id) {
-  let eventObj = {
-    time: id,
-    event: $(`#event${id}`).val(),
-  };
-  //   schedule.push(eventObj);
-  //   alert(JSON.stringify(schedule));
-
-  if (scheduleLocalStorage === null || scheduleLocalStorage === "") {
+  let scheduleLocalStorage = JSON.parse(localStorage.getItem("dailySchedule"));
+  if (scheduleLocalStorage === null || scheduleLocalStorage.length === 0) {
+    let eventObj = {
+      time: id,
+      event: $(`#event${id}`).val(),
+    };
+    console.log("ID is" + id);
     let scheduleData = [
       {
         date: today.format("MM/DD/yyyy"),
@@ -43,14 +75,44 @@ function saveEvent(id) {
       },
     ];
     localStorage.setItem("dailySchedule", JSON.stringify(scheduleData));
+  } else {
+    let daily = scheduleLocalStorage.findIndex(
+      (s) => s.date === today.format("MM/DD/yyyy")
+    );
+    if (daily !== -1) {
+      let eventIndex = scheduleLocalStorage[daily].schedule.findIndex(
+        (s) => s.time === id
+      );
+
+      if (eventIndex !== -1) {
+        scheduleLocalStorage[daily].schedule[eventIndex].event = $(
+          `#event${id}`
+        ).val();
+      } else {
+        scheduleLocalStorage[daily].schedule.push({
+          time: id,
+          event: $(`#event${id}`).val(),
+        });
+      }
+    } else {
+      let eventObj = {
+        time: id,
+        event: $(`#event${id}`).val(),
+      };
+      console.log("ID is" + id);
+      let scheduleData = [
+        {
+          date: today.format("MM/DD/yyyy"),
+          schedule: [eventObj],
+        },
+      ];
+      scheduleLocalStorage.push(scheduleData);
+    }
+    localStorage.setItem("dailySchedule", JSON.stringify(scheduleLocalStorage));
   }
 }
 
+//Initiate document ready function to initate values
 $(document).ready(function () {
   init();
 });
-
-//function to listen for click events on the page
-//grab the saved event options
-//create var to save user input and time
-//save to local storage
